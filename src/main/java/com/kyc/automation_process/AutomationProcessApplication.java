05d50b4296ea2e42ba9bf7d839c26ac125f87954
Ankita Saxena <magix.veritaserum@gmail.com>
2025-02-23 12:57:49 +0100

    Initial Commit

---
+++ src/main/java/com/kyc/automation_process/AutomationProcessApplication.java
@@ -0,0 +1,13 @@
+package com.kyc.automation_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationProcessApplication.class, args);
+	}
+
+}
